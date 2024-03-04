bbdb96d436be1af72b1f8506ab852dc951517f9a
Abefrus <125368781+Abefrus@users.noreply.github.com>
2024-03-04 09:22:43 +0200

    Update pr2.py

--- pr2.py
+++ pr2.py
@@ -1,76 +1,76 @@
-# #1
-# a = 6
-# b = 9
+#1
+a = 6
+b = 9
 
-# print(f'Завдання 1: \n a = {a} \n b = {b}')
+print(f'Завдання 1: \n a = {a} \n b = {b}')
 
-# #2
+ #2
 
-# print(f'Завдання 2: \n', a<b and a>3, b<10 and a < 10, a>b and b==7, b<a and a<2)
+print(f'Завдання 2: \n', a<b and a>3, b<10 and a < 10, a>b and b==7, b<a and a<2)
 
-# #3
-# print(f'Завдання 3: \n', a<b or a>3, b<10 or a < 10, a>b or b<7, b<a or a<2)
+#3
+ print(f'Завдання 3: \n', a<b or a>3, b<10 or a < 10, a>b or b<7, b<a or a<2)
 
-# #4
-# var_str = 'Hello World'
-# var_str2 = 'hello world'
+ #4
+ var_str = 'Hello World'
+ var_str2 = 'hello world'
 
-# print('Завдання 4: \n', var_str==var_str2)
+ print('Завдання 4: \n', var_str==var_str2)
 
-# #5
-# print("Завдання 5: \n")
+#5
+print("Завдання 5: \n")
 
-# x= int(input('Введіть число'))
-# if x > 0:
-#     print('Число більше нуля')
-# if x < 0:
-#     print('Число менше нуля')
+ x= int(input('Введіть число'))
+if x > 0:
+     print('Число більше нуля')
+ if x < 0:
+     print('Число менше нуля')
 
-# #6
-# print('Завдання 6: \n')
+ #6
+ print('Завдання 6: \n')
 
-# if x > 0:
-#     print('1')
-# else:
-#     print('-1')
+if x > 0:
+    print('1')
+else:
+     print('-1')
 
-# #7
-# print('Завдання 7: \n')
+#7
+print('Завдання 7: \n')
 
-# p= int(input('Введіть число'))
-# l= int(input('Введіть число'))
+p= int(input('Введіть число'))
+l= int(input('Введіть число'))
 
-# if p>l:
-#     res= p-l
-# elif p<l:
-#     res = p+l
-# else: 
-#     res = p
+if p>l:
+    res= p-l
+elif p<l:
+    res = p+l
+else: 
+    res = p
 
-# print(res)
+print(res)
 
-# #8
-# print('Завдання 8: \n')
+#8
+print('Завдання 8: \n')
 
-# n = int(input('Введіть ціле число n: '))
+n = int(input('Введіть ціле число n: '))
 
-# if n % 5 == 0:
-#     print('Число кратне 5')
-# else:
-#     print('Число не кратне 5')
+if n % 5 == 0:
+    print('Число кратне 5')
+else:
+    print('Число не кратне 5')
 
 #9
-# fib1 = 0
-# fib2 = 1
-# n = 20
-# i = 3
-# while i <= n:
-#     fib_sum = fib1 + fib2
-#     fib1 = fib2
-#     fib2 = fib_sum
-#     if i >= 5:
-#         print (fib_sum)
-#     i += 1
+fib1 = 0
+fib2 = 1
+n = 20
+i = 3
+while i <= n:
+    fib_sum = fib1 + fib2
+    fib1 = fib2
+    fib2 = fib_sum
+    if i >= 5:
+        print (fib_sum)
+        i += 1
 
 i = 0
 while i <= 20:
